fa086df27e07857651084b6c2572f71446ff8406
tan <haitanle88@gmail.com>
2019-04-16 20:50:54 -0400

    python flask with postgres db posting forum comments
    -include vm installation pdf

---
+++ FSND-Virtual-Machine/vagrant/forum/forumdb.py
@@ -0,0 +1,33 @@
+# "Database code" for the DB Forum.
+
+import datetime
+import psycopg2
+
+# POSTS = [("This is the first post.", datetime.datetime.now())]
+database = "forum"
+
+def get_posts():
+  """Return all posts from the 'database', most recent first."""
+  conn = psycopg2.connect("dbname="+database)
+  cursor = conn.cursor()
+  cursor.execute("select content, time from posts order by time desc;")
+
+  results = cursor.fetchall()  #returns list of tuples
+
+  conn.close()
+
+  return results
+
+def add_post(content):
+  """Add a post to the 'database' with the current timestamp."""
+  conn = psycopg2.connect("dbname="+database)
+  cursor = conn.cursor()
+
+  cursor.execute("insert into posts (content, time ) VALUES (%s, %s)",
+  								(content, datetime.datetime.now()))
+
+  conn.commit()
+  conn.close()
+  #POSTS.append((content, datetime.datetime.now()))
+
+
