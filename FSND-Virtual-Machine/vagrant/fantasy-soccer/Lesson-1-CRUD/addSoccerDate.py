ee79e98f5e4bd7ba5219f7e2ca0d253b5bcc1ff0
tan <haitanle88@gmail.com>
2019-04-24 13:20:58 -0400

    add CRUD read, query, update, delete actions

--- FSND-Virtual-Machine/vagrant/fantasy-soccer/Lesson-1-CRUD/addSoccerDate.py
+++ FSND-Virtual-Machine/vagrant/fantasy-soccer/Lesson-1-CRUD/addSoccerDate.py
@@ -8,16 +8,76 @@ Base.metadata_bind = engine
 DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
+
+#CREATE
+
+
 # juventus = Team(name="Juventus", year_founded=1981)
 
 # ronaldo = Player(name="C Ronaldo", position="striker", age=33, team=juventus )
 
 
 
-barcelona = Team(name="Barcelona", year_founded=1977)
+# barcelona = Team(name="Barcelona", year_founded=1977)
 
-messi = Player(name="Messi", position="striker", age=32, team=barcelona )
+# session.add(Player(name="Busquet", position="mid-fielder", age=30, team=barcelona ))
+# session.add(Player(name="Iniesta", position="mid-fielder", age=35, team=barcelona ))
 
-session.add(barcelona)
-session.add(messi)
-session.commit()
+# session.add(Player(name="Henry", position="striker", age=40, team=barcelona ))
+
+# session.add(Player(name="Puyol", position="defender", age=32, team=barcelona ))
+# session.add(Player(name="Pique", position="defender", age=32, team=barcelona ))
+
+# session.add(Player(name="Xavi", position="mid-fielder", age=32, team=barcelona ))
+
+# session.commit()
+
+
+
+# juventus = Team(name="Barcelona", year_founded=1977)
+
+# session.add(Player(name="Dybala", position="mid-fielder", age=30, team=juventus ))
+# session.add(Player(name="Khediera", position="mid-fielder", age=35, team=juventus ))
+
+# session.add(Player(name="Manduzic", position="striker", age=40, team=juventus ))
+
+# session.add(Player(name="Bonacci", position="defender", age=32, team=juventus ))
+# session.add(Player(name="Chielenni", position="defender", age=32, team=juventus ))
+
+# session.add(Player(name="D Costa", position="mid-fielder", age=32, team=juventus ))
+
+# session.commit()
+
+
+
+#READ 
+
+# teams = session.query(Team).first()
+# print teams.name
+
+
+# players = session.query(Player).all()
+# for player in players:
+# 	print player.name
+
+
+#UPDATE'
+
+# players = session.query(Player).filter_by(team_id = 4).all()
+
+# for player in players:
+# 	player.team_id = 5
+# 	session.add(player)
+
+# session.commit()
+
+
+
+
+
+#DELETE
+
+# item_to_delete = session.query(Player).filter_by(id="1").one()
+
+# session.delete(item_to_delete)
+# session.commit()
