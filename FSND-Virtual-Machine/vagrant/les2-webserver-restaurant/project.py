9708e49a98aa09dd4b059cf262eba07579ef2e1c
tan <haitanle88@gmail.com>
2019-04-24 11:22:36 -0400

    add code to handle POST action to add new restaurant, includes newrestaurantmenu.html template

--- FSND-Virtual-Machine/vagrant/les2-webserver-restaurant/project.py
+++ FSND-Virtual-Machine/vagrant/les2-webserver-restaurant/project.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Restaurant, MenuItem
@@ -8,33 +8,34 @@ app = Flask(__name__)
 engine = create_engine('sqlite:///restaurantmenu.db')
 Base.metadata.bind = engine
 
-DBSession = sessionmaker(bind=engine)
-session = DBSession()
-
 
 @app.route('/')
 def helloWorld():
     return 'hello world'
-    
+
 @app.route('/restaurants/<int:restaurant_id>/')
 def restaurantMenu(restaurant_id):
+
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+
     restaurant = session.query(Restaurant).filter_by(id=restaurant_id).one()
     items = session.query(MenuItem).filter_by(restaurant_id=restaurant.id)
-    output = ''
-    for i in items:
-        output += i.name
-        output += '</br>'
-        output += i.price
-        output += '</br>'
-        output += i.description
-        output += '</br>'
-        output += '</br>'
-    return output
+    return render_template('menu.html', restaurant=restaurant, items=items)
 
 # Task 1: Create route for newMenuItem function here
 
-@app.route('/restaurants/<int:restaurant_id>/create')
+@app.route('/restaurants/<int:restaurant_id>/create', method=['GET', 'POST'])
 def newMenuItem(restaurant_id):
+    if request.method == 'POST':
+        # get form's data
+        request.form['newplayer']
+        #commit/post to the database
+    else: 
+    
+
+
+
     return "page to create a new menu item. Task 1 complete!"
 
 # Task 2: Create route for editMenuItem function here
