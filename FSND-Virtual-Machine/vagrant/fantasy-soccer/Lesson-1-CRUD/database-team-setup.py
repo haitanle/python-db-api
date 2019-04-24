3a722945d0017ac08220fc30065e823389797ba4
tan <haitanle88@gmail.com>
2019-04-24 11:33:44 -0400

    creating database-team-setup.py shell for setting up the db

--- FSND-Virtual-Machine/vagrant/fantasy-soccer/Lesson-1-CRUD/database-team-setup.py
+++ FSND-Virtual-Machine/vagrant/fantasy-soccer/Lesson-1-CRUD/database-team-setup.py
@@ -1,4 +1,14 @@
 import sys
 
-from sqlalcehmy import column, ForeignKey, Integer, String
-from sqlalcehmy.ext.declarative import declarative_base
+from sqlalchemy import column, ForeignKey, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
+from sqlalchemy import create_engine
+
+Base = declarative_base()
+
+
+
+#end of file
+engine = create_engine('sqlite:///fantasySoccerTeam.db')
+Base.metadata.create_all(engine)
