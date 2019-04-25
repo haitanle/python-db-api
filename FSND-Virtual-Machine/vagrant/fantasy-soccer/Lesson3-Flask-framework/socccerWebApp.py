03ea86eb2443e5ba5b6d1a864a8f9265bf276490
tan <haitanle88@gmail.com>
2019-04-25 17:00:25 -0400

    refactor main page to use template

--- FSND-Virtual-Machine/vagrant/fantasy-soccer/Lesson3-Flask-framework/socccerWebApp.py
+++ FSND-Virtual-Machine/vagrant/fantasy-soccer/Lesson3-Flask-framework/socccerWebApp.py
@@ -16,10 +16,7 @@ session = DBSession()
 def show_teams():
 
 	teams = session.query(Team).all()
-	output = "<h1>Champions League!</h1><p>Go to url: /team/{id}/ to see your team:</p>"
-	for team in teams: 
-		output+=("(id: %s) %s %s <br>"%(team.id, team.name, team.year_founded))
-	return output
+	return render_template('main.html', teams=teams)
 
 @app.route('/team/<int:team_id>/', methods=['GET'])
 def team_roster(team_id):
