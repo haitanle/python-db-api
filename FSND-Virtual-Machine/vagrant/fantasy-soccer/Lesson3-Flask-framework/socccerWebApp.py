82648f3467e4f9c4e548a5de083cbdf248b227af
tan <haitanle88@gmail.com>
2019-04-25 16:08:53 -0400

    add players to a team roster

--- FSND-Virtual-Machine/vagrant/fantasy-soccer/Lesson3-Flask-framework/socccerWebApp.py
+++ FSND-Virtual-Machine/vagrant/fantasy-soccer/Lesson3-Flask-framework/socccerWebApp.py
@@ -1,7 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for, request, redirect
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from databaseTeamSetup import Base, Team, Player
+import cgi
 
 app = Flask(__name__)
 
@@ -20,11 +21,26 @@ def show_teams():
 		output+=("(id: %s) %s %s <br>"%(team.id, team.name, team.year_founded))
 	return output
 
-@app.route('/team/<int:team_id>/')
+@app.route('/team/<int:team_id>/', methods=['GET'])
 def team_roster(team_id):
 	team = session.query(Team).filter_by(id=team_id).one()
 	players = session.query(Player).filter_by(team_id=team_id).all()
 	return render_template('team.html', team=team, players=players)
+
+@app.route('/<int:team_id>/add_player', methods=['POST'])
+def add_player_to(team_id):
+	if request.method == 'POST':
+		print('adding player...')
+
+		playerName = request.form['name']
+		playerPosition = request.form['position']
+		playerAge = request.form['age']
+
+		newPlayer = Player(team_id=team_id, name=playerName, position = playerPosition, age=playerAge)
+		session.add(newPlayer)
+		session.commit()
+
+		return redirect(url_for('team_roster',team_id=team_id), code=303)
 
 if __name__ == '__main__':
 	print('startin server...')
